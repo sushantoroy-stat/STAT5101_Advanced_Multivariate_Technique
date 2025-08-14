125a81363c185c7ae449fac6aa546645dedf8b2b
Sushanto Roy <sushantoroy.stat@gmail.com>
2025-08-15 01:34:50 +0600

    Add files via upload

---
+++ 1 Principle Component Analysis/r_file_pca.R
@@ -0,0 +1,34 @@
+# Part 1: Setting up the environment
+install.packages("corrr")
+install.packages("ggcorrplot")
+install.packages("FactoMineR")
+install.packages("factoextra")
+install.packages("broom")
+library("corrr")
+library("ggcorrplot")
+library("FactoMineR")
+library("factoextra")
+library(ggplot2)
+
+
+# Part 2: Recreating the data from the Python example
+data <- data.frame(
+  Height = c(170, 165, 180, 175, 160, 172, 168, 177, 162, 158),
+  Weight = c(65, 59, 75, 68, 55, 70, 62, 74, 58, 54),
+  Age = c(30, 25, 35, 28, 22, 32, 27, 33, 24, 21),
+  Gender = c(1, 0, 1, 1, 0, 1, 0, 1, 0, 0)
+)
+print(data)
+
+# Part 3: Normalizing the data and performing PCA
+numerical_data <- data[, c("Height", "Weight", "Age")]
+data_normalized <- scale(numerical_data)
+data.pca <- princomp(data_normalized)
+
+# Part 4: Displaying and visualizing the results
+summary(data.pca)
+data.pca$loadings[, 1:2]
+fviz_eig(data.pca, addlabels = TRUE)
+fviz_pca_var(data.pca, col.var = "black")
+fviz_cos2(data.pca, choice = "var", axes = 1:2)
+fviz_pca_var(data.pca, col.var = "cos2", gradient.cols = c("black", "orange", "green"), repel = TRUE)
